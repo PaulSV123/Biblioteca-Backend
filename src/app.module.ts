9c59f633373c17e7da81890e11b91409fbe42c6a
Paul <ing12xunder@gmail.com>
2023-05-17 20:08:27 -0500

    Typeorm conexion a BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { EstanteriaModule } from './estanteria/estanteria.module';
 import { ColeccionModule } from './coleccion/coleccion.module';
 import { DesarcargasModule } from './desarcargas/desarcargas.module';
 
 @Module({
-  imports: [EstanteriaModule, ColeccionModule, DesarcargasModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'masterkill',
+      database: 'postgres',
+      entities: [EstanteriaModule, ColeccionModule, DesarcargasModule],
+      synchronize: true,
+    }),
+    EstanteriaModule,
+    ColeccionModule,
+    DesarcargasModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
